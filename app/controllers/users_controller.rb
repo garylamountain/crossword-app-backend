51e5d0923b57ed7627eaa9c2dbac6b09b905e79e
github username <github email address>
2019-11-12 14:11:02 -0600

    added browser tokens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,16 @@ class UsersController < ApplicationController
 
     def create
         user = User.create(username: params[:username], password: params[:password], score: 0)
-        render json: user.to_json
+        user = User.find_by(username: params[:username])
+        payload = { id: user.id}
+    
+        token = JWT.encode payload, 'secret', 'HS256'
+    
+        if user
+          render json: { id: user.id, username: user.username, token: token }
+        else
+          render json: { error: 'invalid credentials' }, status: 401
+        end
     end
 
 end
