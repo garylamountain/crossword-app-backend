0a291c2f4cf00ec5771a4ca634bd53fb2f917d95
github username <github email address>
2019-11-12 16:20:30 -0600

    create method in Games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,11 @@
 class GamesController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        puzzle = Puzzle.find(params[:puzzle])
+        game = Game.create(user: user, puzzle: puzzle)
+        render json: game.to_json
+
+    end
+
 end
